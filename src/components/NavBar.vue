7b0332024ff0011fbf7dc60a38283eaf1309129b
SimanyeSTS <simanyesomdaka@gmail.com>
2024-08-16 16:06:43 +0200

    redefinement

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,17 +1,25 @@
 <template>
-    <div class="container-fluid">
-        <div class="row">
-            <p class="lead">
-                JT &copy; <span id="currYear"></span>
-            </p>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <router-link class="navbar-brand" to="/">
+                <img src="https://simanyests.github.io/Images/images/Screenshot 2024-05-07 141157.png" alt="logo" class="img-fluid"
+                loading="lazy">
+            </router-link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/about">About</router-link>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
+    </nav>
 </template>
-
-<script>
-export default {
-    name: "FooterComp",
-}
-</script>
-
-<style scoped></style>
