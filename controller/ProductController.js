4d382eb7006108ea697cd077bf672381a3da4fa1
SimanyeSTS <simanyesomdaka@gmail.com>
2024-08-16 09:53:55 +0200

    productController

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -1,15 +1,36 @@
 import express from 'express'
-import path from 'path'
+import { verifyAToken } from '../middleware/AuthenticateUser'
 import bodyParser from 'body-parser'
 import { products } from '../model/index.js'
 
-const productsRouter = express.Router()
-productsRouter.use(bodyParser.json())
+const productRouter = express.Router()
 
-productsRouter.get('/', (req, res) => {
+productRouter.use(bodyParser.json())
+
+productRouter.get('/', verifyAToken, (req, res) => {
     products.fetchProducts(req, res)
 })
 
-productsRouter.get('/:id', (req, res) => {
-    products.fetchProducts(req, res)
+productRouter.get('/recent', verifyAToken, (req, res) => {
+    products.fetchProduct(req, res)
 })
+
+productRouter.get('/id:', verifyAToken ,(req, res) => {
+    products.updateProduct(req, res)
+})
+
+productRouter.post('/add', verifyAToken, (req, res) => {
+    products.addProduct(req, res)
+})
+
+productRouter.patch('id:', verifyAToken, (req, res) => {
+    products.updateProduct(req, res)
+})
+
+productRouter.delete('/id:', verifyAToken, (req, res) => {
+    products.deleteProduct(req, res)
+})
+
+export {
+    ProductController
+}
